78974c4eeaf0dddc9e93ce9c2351e29c97a7ad8b
Binh05 <binhdt0452@gmail.com>
2026-04-21 12:09:32 +0700

    Fix: error name env

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-using System.Reflection;
-using System.Text;
-using System.Text.Json.Serialization;
 using FabricIO_api.DataAccess;
 using FabricIO_api.Entities;
 using FabricIO_api.Middleware;
@@ -11,12 +8,20 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
 using Minio;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
@@ -43,17 +48,18 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 // DbContext Config
-builder.Services.AddDbContext<DbContext, AppDbContext>(options =>
+builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    var connectionString = Environment.GetEnvironmentVariable("ASPNETCORE_CONNECTION") ?? builder.Configuration.GetConnectionString("DefaultConnection");
+    var connectionString = Environment.GetEnvironmentVariable("ASPNETCORE_CONNECTION") 
+                           ?? builder.Configuration.GetConnectionString("DefaultConnection");
     options.UseNpgsql(connectionString);
 });
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 //MinIO Config
-var endpoint = Environment.GetEnvironmentVariable("ASPNETCORE_MINIO_ENDPOINT") ?? builder.Configuration["Storage:AccessKey"];
-var accessKey = Environment.GetEnvironmentVariable("ASPNETCORE_MINIO_ACCESSKEY") ?? builder.Configuration["Storage:SecterKey"];
-var secretKeyMinio = Environment.GetEnvironmentVariable("ASPNETCORE_MINIO_SECRETKEY") ?? builder.Configuration["Storage:EndPoint"];
+var accessKey = Environment.GetEnvironmentVariable("ASPNETCORE_MINIO_ENDPOINT") ?? builder.Configuration["Storage:AccessKey"];
+var secretKeyMinio = Environment.GetEnvironmentVariable("ASPNETCORE_MINIO_ACCESSKEY") ?? builder.Configuration["Storage:SecretKey"];
+var endpoint = Environment.GetEnvironmentVariable("ASPNETCORE_MINIO_SECRETKEY") ?? builder.Configuration["Storage:EndPoint"];
 
 builder.Services.AddMinio(configureClient => configureClient
     .WithEndpoint(endpoint)
@@ -116,12 +122,6 @@ builder.Services.AddCors(opt =>
             .AllowCredentials();
     });
 });
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
 
 var app = builder.Build();
 
